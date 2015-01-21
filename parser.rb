f42aa3651320cc626e0f3cad61612ca7fe95d8ee
Jim Sutton <jimsuttonjimsutton@gmail.com>
2015-01-21 12:57:41 -0700

    first commit

--- parser.rb
+++ parser.rb
@@ -1,13 +1,26 @@
 require 'csv'
+require 'json'
+require 'redis-queue'
 
 class Parser
-  def parse(file_path)
+  attr_reader :queue
+
+  QUEUE_NAME = "fetcher_queue" # work queue
+  PROCESSING_QUEUE_NAME = "fetcher_queue_processing" #processing queue
+
+  def initialize
+    @queue = Redis::Queue.new(QUEUE_NAME, PROCESSING_QUEUE_NAME, :redis => Redis.new)
+  end
+
+def parse(file_path)
     rows = CSV.read(file_path)
     headers = rows.shift
     attendees = rows.map do |row|
       headers.zip(row).to_h
     end
-    p attendees.first(10)
+    attendees.first(20).each do |attendee|
+      queue.push(attendee.to_json)
+    end
   end
 end
 
